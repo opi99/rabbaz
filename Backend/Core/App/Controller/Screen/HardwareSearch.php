e889a7a2f1dd1e3e948f9fcbd2ed7d1379d9323d
Alexander Opitz <opitz@extrameile.agency>
2021-03-20 19:19:16 +0100

    Add HardewareSearch / Discovering

--- Backend/Core/App/Controller/Screen/HardwareSearch.php
+++ Backend/Core/App/Controller/Screen/HardwareSearch.php
@@ -4,4 +4,65 @@ namespace Rabbaz\Core\App\Controller\Screen;
 
 class HardwareSearch extends AbstractAdministrationScreen
 {
+    /** @var array Equipments found */
+    protected $equipments = [];
+
+    /**
+     * Control the user input, if available.
+     *
+     * @return boolean True if all user input was accepted.
+     */
+    public function controlInput(): bool
+    {
+        if ($this->getParameter('search')) {
+            $this->searchHardware();
+        }
+        return true;
+    }
+
+    /**
+     * Loads Data for views and defines which views to use.
+     */
+    public function controlView(): bool
+    {
+        $noEquipment = true;
+
+        if (count($this->equipments) > 0) {
+            $view = $this->loadView(\Rabbaz\Core\App\Controller\View\FoundEquipment::class);
+            $view->setFoundEquipment($this->equipments);
+            $this->addView($view);
+
+            $noEquipment = false;
+        }
+
+        $this->application->getTemplater()->setVar('noEquipment', $noEquipment);
+
+        return parent::controlView();
+    }
+
+    protected function searchHardware()
+    {
+        // Load Backend Plugin Registration
+        $pluginRegistration = new \Rabbaz\Core\Plugins\Registration();
+        $pluginRegistration->initPlugins();
+
+        $GLOBALS['pluginRegistration'] = $pluginRegistration;
+
+
+
+
+        // Do scanning
+        $allScanner = $GLOBALS['pluginRegistration']->getPlugins('Scanner');
+
+        foreach ($allScanner as $name => $scannerClass) {
+            $scanner = new $scannerClass();
+            $foundEquipments = $scanner->scan();
+
+            $this->equipments[$scannerClass]['equipment'] = $foundEquipments;
+            $this->equipments[$scannerClass]['errors'] = $scanner->getErrors();
+        }
+
+//         echo $this->showEquipments($equipments);
+
+    }
 }
