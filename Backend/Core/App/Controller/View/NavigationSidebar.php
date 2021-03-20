ed56556c2788fb3171d49710e91eabbf232ba1d0
Alexander Opitz <opitz@extrameile.agency>
2021-03-20 19:17:12 +0100

    Fix comment

--- Backend/Core/App/Controller/View/NavigationSidebar.php
+++ Backend/Core/App/Controller/View/NavigationSidebar.php
@@ -4,7 +4,7 @@ namespace Rabbaz\Core\App\Controller\View;
 
 class NavigationSidebar extends \ForwardFW\Controller\View
 {
-    /** @var strin Name of the screen which is selected */
+    /** @var string Name of the screen which is selected */
     protected $selectedScreenName = '';
 
     public function setSelectedScreenName(string $selectedScreenName): void
