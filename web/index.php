71a6d96ee4e2513ba3a3ea0fee6cc52c53850f95
Alexander Opitz <opitz@extrameile.agency>
2021-03-27 13:38:26 +0100

    Fix path to include

--- web/index.php
+++ web/index.php
@@ -6,8 +6,8 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-include './vendor/autoload.php';
+include '../vendor/autoload.php';
 
 $bootstrap = new ForwardFW\Bootstrap();
-$bootstrap->loadConfig(__DIR__ . '/Backend/Core/App/Config.php');
+$bootstrap->loadConfig(__DIR__ . '/../Backend/Core/App/Config.php');
 $bootstrap->run();
