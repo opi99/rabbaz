715e933fd7560c97ced88b662b9f0b77654a9912
Alexander Opitz <opitz@extrameile.agency>
2021-08-07 12:31:14 +0200

    First part of getting cover files from radio stations (configuration needs to be put into extra file)

--- src/Backend/Core/App/Controller/Screen/DisplayOutput.php
+++ src/Backend/Core/App/Controller/Screen/DisplayOutput.php
@@ -96,7 +96,7 @@ class DisplayOutput extends \ForwardFW\Controller\Screen
     public function getCurrentCover(string $pathAlbum, string $pathOneSong, string $default): string
     {
         $coverHash = md5('coverArt_' . $pathAlbum); //$currentSong['file']));
-        $filename = $this->findCoverFile($coverHash);
+        $filename = false; //$this->findCoverFile($coverHash);
 
         if (!$filename) {
             $filename = $this->retrieveCover($pathOneSong, $coverHash);
@@ -110,6 +110,88 @@ class DisplayOutput extends \ForwardFW\Controller\Screen
     }
 
     public function retrieveCover(string $pathSong, string $coverHash): string
+    {
+        $pathSongConfig = parse_url($pathSong);
+
+        switch ($pathSongConfig['scheme']) {
+            case 'radio':
+                return $this->retrieveRadioCover($pathSongConfig['host'], $coverHash);
+            default:
+                return $this->retrieveMpdCover($pathSong, $coverHash);
+        }
+    }
+
+
+    public function retrieveRadioCover(string $pathSong, string $coverHash): string
+    {
+        $logoConfiguration = [
+            'MDR Sachsen' => 'https://cdn.mdr.de/presse/logos/logo-206-resimage_v-variantBig16x9_w-1472.png?version=10523',
+            'MDR Schlagerwelt' => 'https://cdn.mdr.de/administratives/av/bild-36078_v-variantBig16x9_w-576_zc-915c23fa.png?version=17543',
+            'MDR' => 'https://cdn.mdr.de/presse/logos/mdr-dachmarke-100-resimage_v-variantBig1xN_w-1472.png?version=17833',
+            '104.6' => 'https://www.104.6rtl.com/assets/icons/logo.svg',
+            '1Live' => 'https://www1.wdr.de/radio/1live/1live-logo-schwarz-100~_v-gseagaleriexl.jpg', //'https://www1.wdr.de/resources/img/wdr/logo/epgmodule/1live_logo_claim.svg',
+            '89.0 RTL' => 'https://www.89.0rtl.de/assets/icons/logo.svg',
+            'Absolut Top' => 'https://radioplayer.absolutradio.de/logos/logo_top_500x260.jpg',
+            'Absolut Hot' => 'https://radioplayer.absolutradio.de/logos/logo_hot_500x260.jpg',
+            'Absolut Relax' => 'https://radioplayer.absolutradio.de/logos/logo_relax_500x260.jpg',
+            'Absolut Bella' => 'https://radioplayer.absolutradio.de/logos/logo_bella_500x260.jpg',
+            'Absolut Oldies' => 'https://radioplayer.absolutradio.de/logos/logo_oldies_500x260.jpg',
+            'Absolut musicXL' => 'https://radioplayer.absolutradio.de/logos/logo_musicxl_500x260.jpg',
+            'Alternativ FM' => 'https://www.alternativefm.de/radioplayer/img/plugin-space-image.jpg',
+            'Antenne 1' => 'http://static.radio.de/images/broadcasts/ac/1a/2417/2/c175.png',
+            'Antenne Bayern Chillout' => 'http://static.radio.de/images/broadcasts/21/d5/9117/1/c175.png',
+            'Antenne Bayern' => 'https://www.antenne.de/assets/templates/antenne-de//img/logo-antenne-de-header-2.svg',
+        ];
+        $logoUrl = '';
+
+        // Direct match
+        if (isset($logoConfiguration[$pathSong])) {
+            $logoUrl = $logoConfiguration[$pathSong];
+        }
+
+        if (!$logoUrl) {
+            // Try to match shorter words
+            while ($pos = mb_strrpos($pathSong,' ')) {
+                $pathSong = mb_substr($pathSong, 0, $pos);
+                if (isset($logoConfiguration[$pathSong])) {
+                    $logoUrl = $logoConfiguration[$pathSong];
+                    break;
+                }
+            }
+        }
+
+        if ($logoUrl) {
+            $buffer = file_get_contents($logoUrl);
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mime = $finfo->buffer($buffer);
+
+            $filename = 'Covers/' . $coverHash;
+            switch ($mime) {
+                case 'image/jpeg':
+                    $filename .= '.jpg';
+                    break;
+                case 'image/png':
+                    $filename .= '.png';
+                    break;
+                case 'image/gif':
+                    $filename .= '.gif';
+                    break;
+                case 'image/svg':
+                case 'image/svg+xml':
+                    $filename .= '.svg';
+                    break;
+                default:
+                    // Not supported format like bmp which should be converted before saving
+                    break;
+            }
+
+            file_put_contents($filename, $buffer);
+            return $filename;
+        }
+        return '';
+    }
+
+    public function retrieveMpdCover(string $pathSong, string $coverHash): string
     {
         $coverData = $this->serviceHandler->getCoverFromFile($this->mpdService, $pathSong);
 
@@ -127,6 +209,9 @@ class DisplayOutput extends \ForwardFW\Controller\Screen
                     break;
                 case 'image/gif':
                     $filename .= '.gif';
+                    break;
+                case 'image/svg+xml':
+                    $filename .= '.svg';
                     break;
                 default:
                     // Not supported format like bmp which should be converted before saving
@@ -169,7 +254,7 @@ class DisplayOutput extends \ForwardFW\Controller\Screen
                                 $collection,
                                 [
                                     'path' => $playlistEntrie['file'],
-                                    'cover' => 'Assets/Images/devices/audio-radio.svg',
+                                    'cover' => $this->getCurrentCover($playlistEntrie['file'], 'radio://' . $playlistEntrie['Title'], 'Assets/Images/devices/audio-radio.svg'),
                                     'title' => $playlistEntrie['Title'],
                                     'artist' => (isset($playlistEntrie['Artist'])) ? $playlistEntrie['Artist'] : '',
                                 ]
