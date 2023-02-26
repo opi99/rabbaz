c850d65d7d05c74c662951b0dbc3342db2dba10d
Alexander Opitz <opitz.alexander@googlemail.com>
2023-02-26 19:06:41 +0100

    Update to latest ForwardFW

--- config/Application.php
+++ config/Application.php
@@ -1,57 +1,65 @@
 <?php
 
+declare(strict_types=1);
+
 $templaterConfig = (new ForwardFW\Config\Templater\Twig())
     ->setCompilePath(__DIR__ . '/../cache/')
     ->setTemplatePath(__DIR__ . '/../src/Frontend/Templates');
 
 return (new ForwardFW\Config\Runner())
-        ->addProcessor(
-            (new ForwardFW\Config\Filter\RequestResponse\SimpleRouter())
-                ->addRoute(
-                    (new ForwardFW\Config\Filter\RequestResponse\SimpleRouter\Route())
-                        ->setStart('/administration')
-                        ->addFilter(
-                            (new ForwardFW\Config\Filter\RequestResponse\Application())
-                                ->setConfig(
-                                    (new Rabbaz\Core\App\Config\Application())
-                                        ->setName('Rabbaz Administration')
-                                        ->setScreens([
-                                            'dashboard' => \Rabbaz\Core\App\Controller\Screen\Dashboard::class,
-                                            'displayConfiguration' => \Rabbaz\Core\App\Controller\Screen\DisplayConfiguration::class,
-                                            'hardwareSearch' => \Rabbaz\Core\App\Controller\Screen\HardwareSearch::class,
-                                        ])
-                                        ->setTemplaterConfig($templaterConfig)
-                                )
-                        )
-                )
-                ->addRoute(
-                    (new ForwardFW\Config\Filter\RequestResponse\SimpleRouter\Route())
-                        ->setStart('/display')
-                        ->addFilter(
-                            (new ForwardFW\Config\Filter\RequestResponse\Application())
-                                ->setConfig(
-                                    (new Rabbaz\Core\App\Config\Application())
-                                        ->setName('Rabbaz Displays')
-                                        ->setScreens([
-                                            'displayOutput' => \Rabbaz\Core\App\Controller\Screen\DisplayOutput::class,
-                                        ])
-                                        ->setTemplaterConfig($templaterConfig)
-                                )
-                        )
-                )
-                ->addRoute(
-                    (new ForwardFW\Config\Filter\RequestResponse\SimpleRouter\Route())
-                        ->setStart('/')
-                        ->addFilter(
-                            (new ForwardFW\Config\Filter\RequestResponse\Application())
-                                ->setConfig(
-                                    (new Rabbaz\Core\App\Config\Application())
-                                        ->setName('Hello Rabbaz')
-                                        ->setScreens([
-                                            'Hello' => \Rabbaz\Core\App\Controller\Screen\Hello::class,
-                                        ])
-                                        ->setTemplaterConfig($templaterConfig)
-                                )
-                        )
-                )
-        );
+    ->addService(
+        (new \ForwardFW\Config\Service\Logger\ChromeLogger())
+    )
+    ->addMiddleware(
+        new \ForwardFW\Config\Middleware\ChromeLogger()
+    )
+    ->addMiddleware(
+        (new ForwardFW\Config\Middleware\SimpleRouter())
+            ->addRoute(
+                (new ForwardFW\Config\Middleware\SimpleRouter\Route())
+                    ->setStart('/administration')
+                    ->addMiddleware(
+                        (new ForwardFW\Config\Middleware\Application())
+                            ->setConfig(
+                                (new Rabbaz\Core\App\Config\Application())
+                                    ->setName('Rabbaz Administration')
+                                    ->setScreens([
+                                        'dashboard' => \Rabbaz\Core\App\Controller\Screen\Dashboard::class,
+                                        'displayConfiguration' => \Rabbaz\Core\App\Controller\Screen\DisplayConfiguration::class,
+                                        'hardwareSearch' => \Rabbaz\Core\App\Controller\Screen\HardwareSearch::class,
+                                    ])
+                                    ->setTemplaterConfig($templaterConfig)
+                            )
+                    )
+            )
+            ->addRoute(
+                (new ForwardFW\Config\Middleware\SimpleRouter\Route())
+                    ->setStart('/display')
+                    ->addMiddleware(
+                        (new ForwardFW\Config\Middleware\Application())
+                            ->setConfig(
+                                (new Rabbaz\Core\App\Config\Application())
+                                    ->setName('Rabbaz Displays')
+                                    ->setScreens([
+                                        'displayOutput' => \Rabbaz\Core\App\Controller\Screen\DisplayOutput::class,
+                                    ])
+                                    ->setTemplaterConfig($templaterConfig)
+                            )
+                    )
+            )
+            ->addRoute(
+                (new ForwardFW\Config\Middleware\SimpleRouter\Route())
+                    ->setStart('/')
+                    ->addMiddleware(
+                        (new ForwardFW\Config\Middleware\Application())
+                            ->setConfig(
+                                (new Rabbaz\Core\App\Config\Application())
+                                    ->setName('Hello Rabbaz')
+                                    ->setScreens([
+                                        'Hello' => \Rabbaz\Core\App\Controller\Screen\Hello::class,
+                                    ])
+                                    ->setTemplaterConfig($templaterConfig)
+                            )
+                    )
+            )
+    );
